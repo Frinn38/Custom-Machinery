b8596c41c88505bf987da7420cc3e22ae4b8566b
Frinn38 <32675710+Frinn38@users.noreply.github.com>
2021-04-25 11:47:05 +0200

    Item Requirement support tags now

--- src/main/java/fr/frinn/custommachinery/common/crafting/requirements/ItemRequirement.java
+++ src/main/java/fr/frinn/custommachinery/common/crafting/requirements/ItemRequirement.java
@@ -1,39 +1,74 @@
 package fr.frinn.custommachinery.common.crafting.requirements;
 
+import com.google.gson.JsonParseException;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import fr.frinn.custommachinery.CustomMachinery;
 import fr.frinn.custommachinery.common.crafting.CraftingResult;
 import fr.frinn.custommachinery.common.data.component.MachineComponentType;
 import fr.frinn.custommachinery.common.data.component.handler.ItemComponentHandler;
 import fr.frinn.custommachinery.common.init.Registration;
 import fr.frinn.custommachinery.common.util.Codecs;
+import fr.frinn.custommachinery.common.util.Utils;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.ingredients.IIngredients;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagCollectionManager;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.text.TranslationTextComponent;
 
+import java.util.List;
+import java.util.Optional;
+
 public class ItemRequirement extends AbstractRequirement<ItemComponentHandler> {
+
+    private static final Item DEFAULT_ITEM = Items.AIR;
+    private static final ResourceLocation DEFAULT_TAG = new ResourceLocation(CustomMachinery.MODID, "dummy");
 
     @SuppressWarnings("deprecation")
     public static final Codec<ItemRequirement> CODEC = RecordCodecBuilder.create(itemRequirementInstance ->
             itemRequirementInstance.group(
                     Codecs.REQUIREMENT_MODE_CODEC.fieldOf("mode").forGetter(AbstractRequirement::getMode),
-                    Registry.ITEM.fieldOf("item").forGetter(requirement -> requirement.item),
+                    Registry.ITEM.optionalFieldOf("item", DEFAULT_ITEM).forGetter(requirement -> requirement.item),
+                    ResourceLocation.CODEC.optionalFieldOf("tag", DEFAULT_TAG).forGetter(requirement -> requirement.tag != null ? TagCollectionManager.getManager().getItemTags().getDirectIdFromTag(requirement.tag) : DEFAULT_TAG),
                     Codec.INT.fieldOf("amount").forGetter(requirement -> requirement.amount)
             ).apply(itemRequirementInstance, ItemRequirement::new)
     );
 
     private Item item;
+    private ITag<Item> tag;
     private int amount;
 
-    public ItemRequirement(MODE mode, Item item, int amount) {
+    public ItemRequirement(MODE mode, Item item, ResourceLocation tagLocation, int amount) {
         super(mode);
-        this.item = item;
         this.amount = amount;
+        if(mode == MODE.OUTPUT) {
+            if(item != DEFAULT_ITEM)
+                this.item = item;
+            else throw new IllegalArgumentException("You must specify an item for an Output Item Requirement");
+        } else {
+            if(item == DEFAULT_ITEM) {
+                if(tagLocation == DEFAULT_TAG)
+                    throw  new IllegalArgumentException("You must specify either an item or an item tag for an Input Item Requirement");
+                ITag<Item> tag = TagCollectionManager.getManager().getItemTags().get(tagLocation);
+                if(tag == null)
+                    throw new IllegalArgumentException("The item tag: " + tagLocation + " doesn't exist");
+                if(!tag.getAllElements().isEmpty())
+                    this.tag = tag;
+                else throw new IllegalArgumentException("The item tag: " + tagLocation + " doesn't contains any item");
+            } else {
+                this.item = item;
+            }
+        }
     }
 
     @Override
@@ -49,21 +84,45 @@ public class ItemRequirement extends AbstractRequirement<ItemComponentHandler> {
     @Override
     public boolean test(ItemComponentHandler component) {
         if(getMode() == MODE.INPUT) {
-            return component.getItemAmount(this.item) >= this.amount;
+            if(this.item != null && this.item != DEFAULT_ITEM)
+                return component.getItemAmount(this.item) >= this.amount;
+            else if(this.tag != null)
+                return this.tag.getAllElements().stream().mapToInt(component::getItemAmount).sum() >= this.amount;
+            else throw new IllegalStateException("Using Input Item Requirement with null item and tag");
         } else {
-            return component.getSpaceForItem(this.item) >= this.amount;
+            if(this.item != null && this.item != DEFAULT_ITEM)
+                return component.getSpaceForItem(this.item) >= this.amount;
+            else throw new IllegalStateException("Using Output Item Requirement with null item");
         }
     }
 
     @Override
     public CraftingResult processStart(ItemComponentHandler component) {
         if(getMode() == MODE.INPUT) {
-            int canExtract = component.getItemAmount(this.item);
-            if(canExtract >= this.amount) {
-                component.removeFromInputs(this.item, this.amount);
-                return CraftingResult.success();
-            }
-            return CraftingResult.error(new TranslationTextComponent("custommachinery.requirements.item.error.input", new TranslationTextComponent(this.item.getTranslationKey()), this.amount, canExtract));
+            if(this.item != null && this.item != DEFAULT_ITEM) {
+                int canExtract = component.getItemAmount(this.item);
+                if(canExtract >= this.amount) {
+                    component.removeFromInputs(this.item, this.amount);
+                    return CraftingResult.success();
+                }
+                return CraftingResult.error(new TranslationTextComponent("custommachinery.requirements.item.error.input", new TranslationTextComponent(this.item.getTranslationKey()), this.amount, canExtract));
+            } else if(this.tag != null) {
+                int maxExtract = this.tag.getAllElements().stream().mapToInt(component::getItemAmount).sum();
+                if(maxExtract >= this.amount) {
+                    int toExtract = this.amount;
+                    for (Item item : this.tag.getAllElements()) {
+                        int canExtract = component.getItemAmount(item);
+                        if(canExtract > 0) {
+                            canExtract = Math.min(canExtract, toExtract);
+                            component.removeFromInputs(item, canExtract);
+                            toExtract -= canExtract;
+                            if(toExtract == 0)
+                                return CraftingResult.success();
+                        }
+                    }
+                }
+                return CraftingResult.error(new TranslationTextComponent("custommachinery.requirements.item.error.input", Utils.getItemTagID(this.tag), this.amount, maxExtract));
+            } else throw new IllegalStateException("Using Input Item Requirement with null item and tag");
         }
         return CraftingResult.pass();
     }
--- src/main/java/fr/frinn/custommachinery/common/util/Utils.java
+++ src/main/java/fr/frinn/custommachinery/common/util/Utils.java
@@ -1,6 +1,10 @@
 package fr.frinn.custommachinery.common.util;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.TagCollectionManager;
+import net.minecraft.util.ResourceLocation;
 
 import java.util.Objects;
 
@@ -8,5 +12,9 @@ public class Utils {
 
     public static boolean canPlayerManageMachines(PlayerEntity player) {
         return player.hasPermissionLevel(Objects.requireNonNull(player.getServer()).getOpPermissionLevel());
+    }
+
+    public static ResourceLocation getItemTagID(ITag<Item> tag) {
+        return TagCollectionManager.getManager().getItemTags().getDirectIdFromTag(tag);
     }
 }
