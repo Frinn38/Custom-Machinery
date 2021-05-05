50aaa3769b3b5b65f26f81ed27c8d41c251f91e2
Frinn38 <32675710+Frinn38@users.noreply.github.com>
2021-05-05 13:49:37 +0200

    Make Fuel Requirement check every tick

--- src/main/java/fr/frinn/custommachinery/common/crafting/requirements/FuelRequirement.java
+++ src/main/java/fr/frinn/custommachinery/common/crafting/requirements/FuelRequirement.java
@@ -9,7 +9,7 @@ import fr.frinn.custommachinery.common.data.component.MachineComponentType;
 import fr.frinn.custommachinery.common.init.Registration;
 import net.minecraft.util.text.TranslationTextComponent;
 
-public class FuelRequirement extends AbstractRequirement<FuelMachineComponent> {
+public class FuelRequirement extends AbstractTickableRequirement<FuelMachineComponent> {
 
     public static final Codec<FuelRequirement> CODEC = RecordCodecBuilder.create(fuelRequirementInstance ->
             fuelRequirementInstance.group(
@@ -33,6 +33,13 @@ public class FuelRequirement extends AbstractRequirement<FuelMachineComponent> {
 
     @Override
     public CraftingResult processStart(FuelMachineComponent component) {
+        if(component.isBurning())
+            return CraftingResult.success();
+        return CraftingResult.error(new TranslationTextComponent("custommachinery.requirements.fuel.error"));
+    }
+
+    @Override
+    public CraftingResult processTick(FuelMachineComponent component) {
         if(component.isBurning())
             return CraftingResult.success();
         return CraftingResult.error(new TranslationTextComponent("custommachinery.requirements.fuel.error"));
