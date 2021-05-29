958a8ff70abfcedd4c2d408c7283b3e26e867bfe
Frinn38 <32675710+Frinn38@users.noreply.github.com>
2021-05-29 22:55:37 +0200

    Probably fixed Custom Machine Item rendering crash

--- src/main/java/fr/frinn/custommachinery/client/render/CustomMachineItemRenderer.java
+++ src/main/java/fr/frinn/custommachinery/client/render/CustomMachineItemRenderer.java
@@ -22,22 +22,23 @@ public class CustomMachineItemRenderer extends ItemStackTileEntityRenderer {
     @ParametersAreNonnullByDefault
     @Override
     public void func_239207_a_(ItemStack stack, ItemCameraTransforms.TransformType transformType, MatrixStack matrix, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay) {
-        if(stack.hasTag() && stack.getTag().contains("id", Constants.NBT.TAG_STRING)) {
+        if(stack.getTag() != null && stack.getTag().contains("id", Constants.NBT.TAG_STRING)) {
             ResourceLocation id = new ResourceLocation(stack.getTag().getString("id"));
-            if(!CustomMachinery.MACHINES.containsKey(id) && !MachineLoadingScreen.INSTANCE.getBuilders().containsKey(id))
-                return;
+            CustomMachine machine;
+            if(CustomMachinery.MACHINES.containsKey(id))
+                machine = CustomMachinery.MACHINES.get(id);
+            else if(MachineLoadingScreen.INSTANCE.getBuilders().containsKey(id))
+                machine = MachineLoadingScreen.INSTANCE.getBuilders().get(id).build();
+            else return;
 
-            CustomMachine machine = CustomMachinery.MACHINES.get(id);
-            if(machine != null) {
-                matrix.push();
-                matrix.translate(0.5, 0.5, 0.5);
-                if(transformType.isFirstPerson())
-                    matrix.rotate(Vector3f.YP.rotationDegrees(90));
-                ForgeHooksClient.handleCameraTransforms(matrix, CustomMachineRenderer.getMachineModel(machine.getAppearance()), transformType, transformType == ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND || transformType == ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND);
-                matrix.translate(-0.5, -0.5, -0.5);
-                CustomMachineRenderer.renderMachine(machine, 0, matrix, buffer, combinedLight, combinedOverlay, tintIndex -> Color3F.of(Minecraft.getInstance().getItemColors().getColor(stack, tintIndex)));
-                matrix.pop();
-            }
+            matrix.push();
+            matrix.translate(0.5, 0.5, 0.5);
+            if(transformType.isFirstPerson())
+                matrix.rotate(Vector3f.YP.rotationDegrees(90));
+            ForgeHooksClient.handleCameraTransforms(matrix, CustomMachineRenderer.getMachineModel(machine.getAppearance()), transformType, transformType == ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND || transformType == ItemCameraTransforms.TransformType.FIRST_PERSON_LEFT_HAND);
+            matrix.translate(-0.5, -0.5, -0.5);
+            CustomMachineRenderer.renderMachine(machine, 0, matrix, buffer, combinedLight, combinedOverlay, tintIndex -> Color3F.of(Minecraft.getInstance().getItemColors().getColor(stack, tintIndex)));
+            matrix.pop();
         }
     }
 }
