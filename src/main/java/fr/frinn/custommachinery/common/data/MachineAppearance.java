e5904427a7c17f8c071244f3aa10077d94268a53
Frinn38 <32675710+Frinn38@users.noreply.github.com>
2021-08-22 00:55:59 +0200

    Fix machine item model

--- src/main/java/fr/frinn/custommachinery/common/data/MachineAppearance.java
+++ src/main/java/fr/frinn/custommachinery/common/data/MachineAppearance.java
@@ -5,23 +5,21 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import fr.frinn.custommachinery.api.machine.IMachineAppearance;
 import fr.frinn.custommachinery.client.render.CustomMachineBakedModel;
 import fr.frinn.custommachinery.common.util.Codecs;
-import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.MathHelper;
 
 public class MachineAppearance implements IMachineAppearance {
 
-    public static final ModelResourceLocation DEFAULT_BLOCK_MODEL = new ModelResourceLocation(CustomMachineBakedModel.DEFAULT_MODEL, "");
-    public static final ResourceLocation DEFAULT_ITEM_MODEL = CustomMachineBakedModel.DEFAULT_MODEL;
+    public static final ResourceLocation DEFAULT_MODEL = CustomMachineBakedModel.DEFAULT_MODEL;
     public static final SoundEvent DEFAULT_SOUND = new SoundEvent(new ResourceLocation(""));
     public static final int DEFAULT_LIGHT_LEVEL = 0;
     public static final int DEFAULT_COLOR = 0xFFFFFF;
 
     public static final Codec<MachineAppearance> CODEC = RecordCodecBuilder.create(machineAppearanceCodec ->
             machineAppearanceCodec.group(
-                    Codecs.BLOCK_MODEL_CODEC.optionalFieldOf("block", DEFAULT_BLOCK_MODEL).forGetter(appearance -> appearance.blockModel),
-                    Codecs.ITEM_MODEL_CODEC.optionalFieldOf("item", DEFAULT_ITEM_MODEL).forGetter(appearance -> appearance.itemModel),
+                    Codecs.BLOCK_MODEL_CODEC.optionalFieldOf("block", DEFAULT_MODEL).forGetter(appearance -> appearance.blockModel),
+                    Codecs.ITEM_MODEL_CODEC.optionalFieldOf("item", DEFAULT_MODEL).forGetter(appearance -> appearance.itemModel),
                     SoundEvent.CODEC.optionalFieldOf("sound", DEFAULT_SOUND).forGetter(appearance -> appearance.sound),
                     Codec.INT.optionalFieldOf("lightlevel", DEFAULT_LIGHT_LEVEL).forGetter(appearance -> appearance.lightLevel),
                     Codec.INT.optionalFieldOf("color", DEFAULT_COLOR).forGetter(appearance -> appearance.color)
@@ -29,7 +27,7 @@ public class MachineAppearance implements IMachineAppearance {
     );
 
 
-    public static final MachineAppearance DEFAULT = new MachineAppearance(DEFAULT_BLOCK_MODEL, DEFAULT_ITEM_MODEL, DEFAULT_SOUND, DEFAULT_LIGHT_LEVEL, DEFAULT_COLOR);
+    public static final MachineAppearance DEFAULT = new MachineAppearance(DEFAULT_MODEL, DEFAULT_MODEL, DEFAULT_SOUND, DEFAULT_LIGHT_LEVEL, DEFAULT_COLOR);
 
     private ResourceLocation blockModel;
     private ResourceLocation itemModel;
@@ -39,7 +37,10 @@ public class MachineAppearance implements IMachineAppearance {
 
     public MachineAppearance(ResourceLocation blockModel, ResourceLocation itemModel, SoundEvent sound, int lightLevel, int color) {
         this.blockModel = blockModel;
-        this.itemModel = itemModel;
+        if(blockModel != DEFAULT_MODEL && itemModel == DEFAULT_MODEL)
+            this.itemModel = blockModel;
+        else
+            this.itemModel = itemModel;
         this.sound = sound;
         this.lightLevel = MathHelper.clamp(lightLevel, 0, 15);
         this.color = color;
