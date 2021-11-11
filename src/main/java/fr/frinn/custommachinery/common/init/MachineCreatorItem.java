7320f774e6e8959a4c2e8d506ac9f7aaf41d08ff
Frinn38 <32675710+Frinn38@users.noreply.github.com>
2021-11-11 17:42:36 +0100

    Add warning to machine creator item

--- src/main/java/fr/frinn/custommachinery/common/init/MachineCreatorItem.java
+++ src/main/java/fr/frinn/custommachinery/common/init/MachineCreatorItem.java
@@ -1,13 +1,22 @@
 package fr.frinn.custommachinery.common.init;
 
 import fr.frinn.custommachinery.client.ClientHandler;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
+
+@ParametersAreNonnullByDefault
 public class MachineCreatorItem extends Item {
 
     public MachineCreatorItem(Properties properties) {
@@ -19,5 +28,10 @@ public class MachineCreatorItem extends Item {
         if(world.isRemote())
             ClientHandler.openMachineLoadingScreen();
         return super.onItemRightClick(world, player, hand);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltips, ITooltipFlag flagIn) {
+        tooltips.add(new TranslationTextComponent("custommachinery.machine_creator.warning").mergeStyle(TextFormatting.DARK_RED));
     }
 }
