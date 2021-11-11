90f05dc9eac9dca913c247a0a0a1f81ab9747622
Frinn38 <32675710+Frinn38@users.noreply.github.com>
2021-11-11 14:30:14 +0100

    Fix energy bar in jei shifted

--- src/main/java/fr/frinn/custommachinery/client/render/element/jei/EnergyJEIIngredientRenderer.java
+++ src/main/java/fr/frinn/custommachinery/client/render/element/jei/EnergyJEIIngredientRenderer.java
@@ -1,8 +1,6 @@
 package fr.frinn.custommachinery.client.render.element.jei;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
-import fr.frinn.custommachinery.CustomMachinery;
 import fr.frinn.custommachinery.api.guielement.jei.JEIIngredientRenderer;
 import fr.frinn.custommachinery.common.data.gui.EnergyGuiElement;
 import fr.frinn.custommachinery.common.integration.jei.CustomIngredientTypes;
@@ -11,12 +9,7 @@ import mezz.jei.api.MethodsReturnNonnullByDefault;
 import mezz.jei.api.ingredients.IIngredientType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldVertexBufferUploader;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -29,8 +22,6 @@ import java.util.List;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class EnergyJEIIngredientRenderer extends JEIIngredientRenderer<Energy, EnergyGuiElement> {
-
-    private static final ResourceLocation EMPTY = new ResourceLocation(CustomMachinery.MODID, "textures/gui/base_empty.png");
 
     public EnergyJEIIngredientRenderer(EnergyGuiElement element) {
         super(element);
@@ -45,14 +36,11 @@ public class EnergyJEIIngredientRenderer extends JEIIngredientRenderer<Energy, E
     public void render(MatrixStack matrix, int x, int y, EnergyGuiElement element, @Nullable Energy ingredient) {
         int width = element.getWidth();
         int height = element.getHeight();
-        Minecraft.getInstance().getTextureManager().bindTexture(element.getEmptyTexture());
-        AbstractGui.blit(matrix, x - 1, y - 1, 0, 0, width, height, width, height);
-        if(ingredient != null) {
-            double fillPercent = 1.0D;
-            int eneryHeight = (int)(fillPercent * (double)(height - 2));
+        if(ingredient != null)
             Minecraft.getInstance().getTextureManager().bindTexture(element.getFilledTexture());
-            AbstractGui.blit(matrix, x, y + height - eneryHeight, 0, height - eneryHeight, width, eneryHeight, width, height);
-        }
+        else
+            Minecraft.getInstance().getTextureManager().bindTexture(element.getEmptyTexture());
+        AbstractGui.blit(matrix, -1, -1, 0, 0, width, height, width, height);
     }
 
     @Override
