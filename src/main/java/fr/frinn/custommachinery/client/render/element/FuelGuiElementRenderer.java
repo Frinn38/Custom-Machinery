ced994e58928a26d15cefb1faec24b7509e39514
Frinn38 <32675710+Frinn38@users.noreply.github.com>
2021-12-27 14:18:23 +0100

    Fuel element render in jei now

--- src/main/java/fr/frinn/custommachinery/client/render/element/FuelGuiElementRenderer.java
+++ src/main/java/fr/frinn/custommachinery/client/render/element/FuelGuiElementRenderer.java
@@ -1,16 +1,22 @@
 package fr.frinn.custommachinery.client.render.element;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import fr.frinn.custommachinery.api.crafting.IMachineRecipe;
 import fr.frinn.custommachinery.api.guielement.IGuiElementRenderer;
 import fr.frinn.custommachinery.api.guielement.IMachineScreen;
+import fr.frinn.custommachinery.api.integration.jei.IJEIElementRenderer;
 import fr.frinn.custommachinery.common.data.component.FuelMachineComponent;
 import fr.frinn.custommachinery.common.data.gui.FuelGuiElement;
 import fr.frinn.custommachinery.common.init.Registration;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 
-public class FuelGuiElementRenderer implements IGuiElementRenderer<FuelGuiElement> {
+import java.util.Collections;
+import java.util.List;
+
+public class FuelGuiElementRenderer implements IGuiElementRenderer<FuelGuiElement>, IJEIElementRenderer<FuelGuiElement> {
 
     @Override
     public void renderElement(MatrixStack matrix, FuelGuiElement element, IMachineScreen screen) {
@@ -36,11 +42,22 @@ public class FuelGuiElementRenderer implements IGuiElementRenderer<FuelGuiElemen
     }
 
     @Override
-    public boolean isHovered(FuelGuiElement element, IMachineScreen screen, int mouseX, int mouseY) {
+    public void renderElementInJEI(MatrixStack matrix, FuelGuiElement element, IMachineRecipe recipe, int mouseX, int mouseY) {
+        if(Minecraft.getInstance().world == null)
+            return;
         int posX = element.getX();
         int posY = element.getY();
         int width = element.getWidth();
         int height = element.getHeight();
-        return mouseX >= posX && mouseX <= posX + width && mouseY >= posY && mouseY <= posY + height;
+        Minecraft.getInstance().getTextureManager().bindTexture(element.getEmptyTexture());
+        AbstractGui.blit(matrix, posX, posY, 0, 0, width, height, width, height);
+        int filledHeight = (int)(Minecraft.getInstance().world.getGameTime() / 2 % height);
+        Minecraft.getInstance().textureManager.bindTexture(element.getFilledTexture());
+        AbstractGui.blit(matrix, posX, posY + height - filledHeight, 0, height - filledHeight, width, filledHeight, width, height);
+    }
+
+    @Override
+    public List<ITextComponent> getJEITooltips(FuelGuiElement element, IMachineRecipe recipe) {
+        return Collections.emptyList();
     }
 }
