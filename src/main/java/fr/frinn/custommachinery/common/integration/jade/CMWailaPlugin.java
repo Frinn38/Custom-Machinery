9285c7efb7ac9be749e4186033d4ee521986f2f6
Frinn38 <francois.moreau3@aliceadsl.fr>
2022-06-06 18:45:49 +0200

    Improve jade integration to display status, recipe progress and error.

--- src/main/java/fr/frinn/custommachinery/common/integration/jade/CMWailaPlugin.java
+++ src/main/java/fr/frinn/custommachinery/common/integration/jade/CMWailaPlugin.java
@@ -1,8 +1,12 @@
 package fr.frinn.custommachinery.common.integration.jade;
 
+import fr.frinn.custommachinery.common.init.CustomMachineBlock;
+import fr.frinn.custommachinery.common.init.CustomMachineTile;
 import fr.frinn.custommachinery.common.init.Registration;
 import mcp.mobius.waila.api.IWailaClientRegistration;
+import mcp.mobius.waila.api.IWailaCommonRegistration;
 import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.TooltipPosition;
 import mcp.mobius.waila.api.WailaPlugin;
 
 @WailaPlugin
@@ -11,6 +15,13 @@ public class CMWailaPlugin implements IWailaPlugin {
     @SuppressWarnings("UnstableApiUsage")
     @Override
     public void registerClient(IWailaClientRegistration registration) {
+        registration.registerComponentProvider(CustomMachineComponentProvider.INSTANCE, TooltipPosition.BODY, CustomMachineBlock.class);
         registration.usePickedResult(Registration.CUSTOM_MACHINE_BLOCK.get());
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    @Override
+    public void register(IWailaCommonRegistration registration) {
+        registration.registerBlockDataProvider(CustomMachineServerDataProvider.INSTANCE, CustomMachineTile.class);
     }
 }
