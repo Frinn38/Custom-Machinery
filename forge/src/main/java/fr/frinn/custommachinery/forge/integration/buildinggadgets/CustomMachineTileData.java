aa41f4afa3a3f64c64f6db631fc2c2c2f467f461
Frinn38 <francois.moreau3@aliceadsl.fr>
2022-10-15 16:53:09 +0200

    Fix crash with building gadget integration (unchecked cast)

--- forge/src/main/java/fr/frinn/custommachinery/forge/integration/buildinggadgets/CustomMachineTileData.java
+++ forge/src/main/java/fr/frinn/custommachinery/forge/integration/buildinggadgets/CustomMachineTileData.java
@@ -6,7 +6,7 @@ import com.direwolf20.buildinggadgets.common.tainted.building.view.BuildContext;
 import fr.frinn.custommachinery.common.init.CustomMachineTile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -31,14 +31,13 @@ public class CustomMachineTileData implements ITileEntityData {
 
     @Override
     public boolean placeIn(BuildContext context, BlockState state, BlockPos pos) {
-        Level world = context.getServerWorld();
+        LevelAccessor world = context.getWorld();
 
         world.setBlock(pos, state, Block.UPDATE_ALL);
-        world.getBlockState(pos).neighborChanged(world, pos, world.getBlockState(pos.above()).getBlock(), pos.above(), false);
         Optional.ofNullable(world.getBlockEntity(pos))
                 .filter(tile -> tile instanceof CustomMachineTile)
                 .map(tile -> (CustomMachineTile)tile)
-                .ifPresent(machine -> machine.setId(machineID));
+                .ifPresent(machine -> machine.setId(this.machineID));
 
         return true;
     }
