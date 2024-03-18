0bc5cd511624d3049a4667319fc3db82625d7a07
Frinn <francois.moreaumazuet@gmail.com>
2024-03-18 18:47:43 +0100

    Add cm reload command

--- common/src/main/java/fr/frinn/custommachinery/common/command/CMCommand.java
+++ common/src/main/java/fr/frinn/custommachinery/common/command/CMCommand.java
@@ -2,18 +2,24 @@ package fr.frinn.custommachinery.common.command;
 
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import fr.frinn.custommachinery.common.machine.CustomMachineJsonReloadListener;
 import fr.frinn.custommachinery.common.network.SOpenFilePacket;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.profiling.InactiveProfiler;
 
 import java.io.File;
+import java.util.concurrent.CompletableFuture;
 
 public class CMCommand {
 
     public static LiteralArgumentBuilder<CommandSourceStack> register(String name) {
         return Commands.literal(name)
-                .then(logging());
+                .then(logging())
+                .then(reload());
     }
 
     private static ArgumentBuilder<CommandSourceStack, ?> logging() {
@@ -22,6 +28,17 @@ public class CMCommand {
                 .executes(ctx -> {
                     if(ctx.getSource().getEntity() instanceof ServerPlayer player)
                         new SOpenFilePacket(new File("logs/custommachinery.log").toURI().toString()).sendTo(player);
+                    return 0;
+                });
+    }
+
+    private static ArgumentBuilder<CommandSourceStack, ?> reload() {
+        return Commands.literal("reload")
+                .requires(cs -> cs.hasPermission(2))
+                .executes(ctx -> {
+                    if(ctx.getSource().getEntity() instanceof ServerPlayer player)
+                        new CustomMachineJsonReloadListener().reload(CompletableFuture::completedFuture, player.getServer().getResourceManager(), InactiveProfiler.INSTANCE, InactiveProfiler.INSTANCE, player.getServer(), player.getServer())
+                                .thenRun(() -> player.sendSystemMessage(Component.translatable("custommachinery.command.reload").withStyle(ChatFormatting.GRAY)));
                     return 0;
                 });
     }
