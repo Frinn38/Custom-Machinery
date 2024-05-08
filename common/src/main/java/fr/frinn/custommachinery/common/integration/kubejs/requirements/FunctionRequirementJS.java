3d3b43505b5e2c50a1ae7942922507932d36ac30
Frinn38 <francois.moreaumazuet@gmail.com>
2024-05-08 17:03:55 +0200

    Fix crash when there is an error in a function requirement script.

--- common/src/main/java/fr/frinn/custommachinery/common/integration/kubejs/requirements/FunctionRequirementJS.java
+++ common/src/main/java/fr/frinn/custommachinery/common/integration/kubejs/requirements/FunctionRequirementJS.java
@@ -1,5 +1,6 @@
 package fr.frinn.custommachinery.common.integration.kubejs.requirements;
 
+import dev.latvian.mods.kubejs.script.ScriptType;
 import fr.frinn.custommachinery.api.integration.kubejs.RecipeJSBuilder;
 import fr.frinn.custommachinery.common.integration.kubejs.function.KJSFunction;
 import fr.frinn.custommachinery.common.integration.kubejs.function.RecipeFunction;
@@ -8,18 +9,18 @@ import fr.frinn.custommachinery.common.requirement.FunctionRequirement;
 public interface FunctionRequirementJS extends RecipeJSBuilder {
 
     default RecipeJSBuilder requireFunctionToStart(RecipeFunction function) {
-        return this.addRequirement(new FunctionRequirement(FunctionRequirement.Phase.CHECK, new KJSFunction(function)));
+        return this.addRequirement(new FunctionRequirement(FunctionRequirement.Phase.CHECK, new KJSFunction(function), error -> ScriptType.SERVER.console.error(error.getLocalizedMessage())));
     }
 
     default RecipeJSBuilder requireFunctionOnStart(RecipeFunction function) {
-        return this.addRequirement(new FunctionRequirement(FunctionRequirement.Phase.START, new KJSFunction(function)));
+        return this.addRequirement(new FunctionRequirement(FunctionRequirement.Phase.START, new KJSFunction(function), error -> ScriptType.SERVER.console.error(error.getLocalizedMessage())));
     }
 
     default RecipeJSBuilder requireFunctionEachTick(RecipeFunction function) {
-        return this.addRequirement(new FunctionRequirement(FunctionRequirement.Phase.TICK, new KJSFunction(function)));
+        return this.addRequirement(new FunctionRequirement(FunctionRequirement.Phase.TICK, new KJSFunction(function), error -> ScriptType.SERVER.console.error(error.getLocalizedMessage())));
     }
 
     default RecipeJSBuilder requireFunctionOnEnd(RecipeFunction function) {
-        return this.addRequirement(new FunctionRequirement(FunctionRequirement.Phase.END, new KJSFunction(function)));
+        return this.addRequirement(new FunctionRequirement(FunctionRequirement.Phase.END, new KJSFunction(function), error -> ScriptType.SERVER.console.error(error.getLocalizedMessage())));
     }
 }
