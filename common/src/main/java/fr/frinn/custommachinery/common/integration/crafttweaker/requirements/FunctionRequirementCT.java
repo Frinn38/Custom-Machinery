e4548af8e02601edcc063085b4b8a78972bee145
Frinn38 <francois.moreaumazuet@gmail.com>
2024-06-20 00:25:02 +0200

    Fix CT error

--- common/src/main/java/fr/frinn/custommachinery/common/integration/crafttweaker/requirements/FunctionRequirementCT.java
+++ common/src/main/java/fr/frinn/custommachinery/common/integration/crafttweaker/requirements/FunctionRequirementCT.java
@@ -2,6 +2,7 @@ package fr.frinn.custommachinery.common.integration.crafttweaker.requirements;
 
 import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import fr.frinn.custommachinery.CustomMachinery;
 import fr.frinn.custommachinery.api.crafting.CraftingResult;
 import fr.frinn.custommachinery.api.integration.crafttweaker.RecipeCTBuilder;
 import fr.frinn.custommachinery.common.integration.crafttweaker.CTConstants;
@@ -19,21 +20,21 @@ public interface FunctionRequirementCT<T> extends RecipeCTBuilder<T> {
 
     @Method
     default T requireFunctionToStart(Function<Context, CraftingResult> function) {
-        return addRequirement(new FunctionRequirement(FunctionRequirement.Phase.CHECK, new CTFunction(function), CraftTweakerAPI.LOGGER::throwing));
+        return addRequirement(new FunctionRequirement(FunctionRequirement.Phase.CHECK, new CTFunction(function), CraftTweakerAPI.getLogger(CustomMachinery.MODID)::throwing));
     }
 
     @Method
     default T requireFunctionOnStart(Function<Context, CraftingResult> function) {
-        return addRequirement(new FunctionRequirement(FunctionRequirement.Phase.START, new CTFunction(function), CraftTweakerAPI.LOGGER::throwing));
+        return addRequirement(new FunctionRequirement(FunctionRequirement.Phase.START, new CTFunction(function), CraftTweakerAPI.getLogger(CustomMachinery.MODID)::throwing));
     }
 
     @Method
     default T requireFunctionEachTick(Function<Context, CraftingResult> function) {
-        return addRequirement(new FunctionRequirement(FunctionRequirement.Phase.TICK, new CTFunction(function), CraftTweakerAPI.LOGGER::throwing));
+        return addRequirement(new FunctionRequirement(FunctionRequirement.Phase.TICK, new CTFunction(function), CraftTweakerAPI.getLogger(CustomMachinery.MODID)::throwing));
     }
 
     @Method
     default T requireFunctionOnEnd(Function<Context, CraftingResult> function) {
-        return addRequirement(new FunctionRequirement(FunctionRequirement.Phase.END, new CTFunction(function), CraftTweakerAPI.LOGGER::throwing));
+        return addRequirement(new FunctionRequirement(FunctionRequirement.Phase.END, new CTFunction(function), CraftTweakerAPI.getLogger(CustomMachinery.MODID)::throwing));
     }
 }
