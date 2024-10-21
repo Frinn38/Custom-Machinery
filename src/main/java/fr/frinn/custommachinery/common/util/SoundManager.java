b831437e1b67d60631e2a88493238dbca435937b
Frinn38 <francois.moreaumazuet@gmail.com>
2024-10-21 17:19:45 +0200

    Fix ambient sound not looping properly (micro-pause before playing again)

--- src/main/java/fr/frinn/custommachinery/common/util/SoundManager.java
+++ src/main/java/fr/frinn/custommachinery/common/util/SoundManager.java
@@ -3,6 +3,7 @@ package fr.frinn.custommachinery.common.util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance.Attenuation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
@@ -39,7 +40,7 @@ public class SoundManager {
             return;
         }
 
-        this.sound = new SimpleSoundInstance(sound, SoundSource.BLOCKS, 1.0F, 1.0F, RandomSource.create(), this.pos);
+        this.sound = new SimpleSoundInstance(sound.getLocation(), SoundSource.BLOCKS, 1.0F, 1.0F, RandomSource.create(), true, 0, Attenuation.LINEAR, this.pos.getX(), this.pos.getY(), this.pos.getZ(), false);
         play();
     }
 
